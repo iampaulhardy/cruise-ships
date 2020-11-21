0c18f830288e090627b27efb18d1db94ae2046dd
Paul Hardy <pauljosephhardy@gmail.com>
2020-11-21 15:08:06 +0000

    Make it Dry

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -6,30 +6,37 @@ const Ship = require('../src/ship.js');
 describe('Port', () => {
   it('can be instantiated', () => {
     expect(new Port()).toBeInstanceOf(Object);
-  });
+    });
+      describe('Ports can have a name and ships', () => {
+        let port;
+        let ship;
+        let titanic;
+        let queenMary;
+        let blackPearl;
+        beforeEach(() => {
+          port = new Port('Dover');
+          ship = {};
+          titanic = {};
+          queenMary = {};
+          blackPearl = {};
+        })
 
-  it('can have a name', () => {
-    const port = new Port('Southampton')
-    expect(port.name).toBe('Southampton')
-  });
+      it('can have a name', () => {
+        expect(port.name).toBe('Dover')
+      });
 
-  it('can add a ship', () => {
-    const port = new Port ('Dover');
-    const ship = {};
-    port.addShip(ship);
-    expect(port.ships).toContain(ship);
-  });
+      it('can add a ship', () => {
+        port.addShip(ship);
+        expect(port.ships).toContain(ship);
+      });
 
-  it('can remove a ship', () => {
-    const port = new Port ('Rotterdam');
-    const titanic = {};
-    const queenMary = {};
-    const blackPearl = {};
-    port.addShip(titanic);
-    port.addShip(queenMary);
-    port.addShip(blackPearl);
-    port.removeShip(titanic);
-    port.removeShip(queenMary)
-    expect(port.ships).toEqual([blackPearl]);
+      it('can remove a ship', () => {
+        port.addShip(titanic);
+        port.addShip(queenMary);
+        port.addShip(blackPearl);
+        port.removeShip(titanic);
+        port.removeShip(queenMary)
+        expect(port.ships).toEqual([blackPearl]);
+      });
+    });
   });
-});
